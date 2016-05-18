d80fa151630edd4faa97fded20097fcecf334311
Ryan Ingram <ry_ingram@hotmail.com>
2016-05-17 22:52:39 -0400

    Initial Commit of 3 projects

---
+++ Temp10Minimal/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Temp10Minimal.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Temp10Minimal.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ Temp10Minimal/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Temp10Minimal.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Temp10Minimal.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
